6eb7c66cfe46e6884560ac5e28271d37612d7e9a
Stanley Situ <ssitu@dayspring-tech.com>
2017-12-05 10:47:25 -0800

    add firebase module to angular project

--- src/app/components/share-pages/share-pages.module.ts
+++ src/app/components/share-pages/share-pages.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { BrowserModule } from '@angular/platform-browser';
 import { LOG_LOGGER_PROVIDERS } from 'angular2-logger/core';
 
@@ -7,11 +10,15 @@ import { ShareFormComponent } from './share-form.component';
 import { ConfirmationComponent } from './confirmation.component';
 import { ShareFormService } from '../../services/share-form.service';
 
+import { environment } from '../../environments/firebase.environment';
 
 @NgModule({
     imports: [
         BrowserModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireDatabaseModule,
+        AngularFireAuthModule
     ],
     exports: [
         ShareFormComponent
---
+++ src/app/environments/firebase.environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDA97-rzb17moFJsuuDcScWdzBsB37nT6o",
+        authDomain: "one-time-share.firebaseapp.com",
+        databaseURL: "https://one-time-share.firebaseio.com",
+        projectId: "one-time-share",
+        storageBucket: "one-time-share.appspot.com",
+        messagingSenderId: "560160661773"
+    }
+};
